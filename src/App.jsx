c5bc1b30cab53c4af09dbe5cc2aa3f978e5a3351
Shaiful <shaiful48027@gmail.com>
2024-12-18 11:54:53 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
+import Blog from './components/Blog/Blog'
 import Blogs from './components/Blogs/Blogs'
+import BookMarks from './components/Bookmarks/BookMarks'
 import Header from './components/header/Header'
 
 function App() {
@@ -9,12 +11,12 @@ function App() {
 
 
       <Header></Header>
-      <Blogs></Blogs>
-
-
-
-
-
+      
+      <div className='md:flex mx-8'>
+        <Blogs></Blogs>
+        <BookMarks></BookMarks>
+        <Blog></Blog>
+      </div>
 
 
     </>
